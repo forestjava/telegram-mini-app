a201518db61e7d94c2b57e325d04d3d2fccf0bb2
Nikita Biziaev <cap7goo@gmail.com>
2026-01-18 23:15:19 +0500

    test #2315

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,17 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { QueryClientProvider } from '@tanstack/react-query'
-// import { queryClient } from './api/queryClient'
-// import { AuthProvider } from './context/AuthContext.tsx'
-// import './index.css'
-// import App from './App.tsx'
-
-const Test = () => {
-  return <div>Test</div>
-}
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './api/queryClient'
+import { AuthProvider } from './context/AuthContext.tsx'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  //   <QueryClientProvider client={queryClient}>
-  //     <AuthProvider>
-  //       <App />
-  //     </AuthProvider>
-  //   </QueryClientProvider>
-  // </StrictMode>,
-  <Test />
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>,
 )
